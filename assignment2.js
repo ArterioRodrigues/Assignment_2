8675ea0c60654a5efd4929337fae4553652a9322
ArterioRodrigues <arterio369@gmail.com>
2021-09-25 19:47:43 -0400

    MyMap is finished

--- assignment2.js
+++ assignment2.js
@@ -1,11 +1,22 @@
 // FOR EACH //
-Array.prototype.myEach = function() {
-
+Array.prototype.myEach = function(callbackFn) {
+    for(let i = 0 ; i < this.length; i++)
+    {
+        if(this[i] === undefined) continue;
+        callbackFn(this[i], i , this);
+    }
 };
 
 // MAP //
-Array.prototype.myMap = function() {
+Array.prototype.myMap = function(callbackFn) {
+    let arr = [];
 
+    for(let i = 0 ; i < this.length; i++)
+    {
+        if(this[i] === undefined) continue;
+        arr.push(callbackFn(this[i], i , this));
+    }
+    return arr;
 };
 
 // FILTER //
---
+++ myMap.js
@@ -0,0 +1,22 @@
+// MAP //
+Array.prototype.myMap = function(callbackFn) {
+    let arr = [];
+
+    for(let i = 0 ; i < this.length; i++)
+    {
+        if(this[i] === undefined) continue;
+
+       arr.push(callbackFn(this[i], i , this));
+    }
+
+    return arr;
+};
+ 
+let numbers = [1, 2, 3, 4]
+let filteredNumbers = numbers.map(function(num, index) {
+  if (index < 3) {
+     return num
+  }
+})
+
+console.log(filteredNumbers);
